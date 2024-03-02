20cd35281d4e6893cd453b5b2ede577f55f45d9e
Qumber Rizvi <qumberrizvi@ymail.com>
2024-03-02 15:30:52 +0400

    Google Analytics type declaration

--- src/app/services/google-analytics.service.ts
+++ src/app/services/google-analytics.service.ts
@@ -2,8 +2,6 @@ import {Injectable} from "@angular/core";
 import {NavigationEnd, Router} from "@angular/router";
 import {environment} from "../../environments/environment";
 
-declare var gtag: Function;
-
 @Injectable({providedIn: 'root'})
 export class GoogleAnalyticsService {
     constructor(private router: Router) {
